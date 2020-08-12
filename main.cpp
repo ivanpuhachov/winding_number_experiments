aed8b36e17b542ece614653aa55c7c62091bbcba
ivan <ivan.puhachov@gmail.com>
2020-08-11 21:27:43 -0400

    console args

--- main.cpp
+++ main.cpp
@@ -18,6 +18,11 @@
 
 int main(int argc, char *argv[])
 {
+    std::string filename = "../bathtub.obj";
+    if (argc > 1){
+        filename = argv[1];
+        std::cout << filename << std::endl;
+    }
     const auto time = [](std::function<void(void)> func)->double
     {
         const double t_before = igl::get_seconds();
@@ -29,7 +34,7 @@ int main(int argc, char *argv[])
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
 //    igl::read_triangle_mesh("../bunny.off",V,F);
-    igl::read_triangle_mesh("../bathtub.obj", V,F);
+    igl::read_triangle_mesh(filename, V,F);
 
     // Normalize to unit sphere
     const Eigen::RowVector3d minDims = V.colwise().minCoeff();
